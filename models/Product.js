317512fcca4e0bdca1c573d5123d9ea6547de046
Jazyel99 <jaziel@outlook.com>
2022-08-31 10:56:29 -0300

    fix: fixed return getProductByID

--- models/Product.js
+++ models/Product.js
@@ -4,9 +4,9 @@ const selectProductByIDQuery = `SELECT * FROM
 StoreManager.products WHERE id=?`;
 
 const productModel = {
-  // ::DOING pegar produto pelo ID
+  // ::DONE pegar produto pelo ID
   getProductByID: async (productID) => {
-    const [[result]] = await connection.execute(selectProductByIDQuery, [productID]);
+    const [result] = await connection.execute(selectProductByIDQuery, [productID]);
     return result;
   },
   // :: TODO pegar todos os produtos
