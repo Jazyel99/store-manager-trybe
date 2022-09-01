0b58a42ed14dce17609758b17cded846469b96bf
Jazyel99 <jaziel@outlook.com>
2022-09-01 20:27:14 -0300

    feat: add getSaleById in models layer

--- models/Sale.js
+++ models/Sale.js
@@ -1,15 +1,22 @@
 const connection = require('./config/connection');
 
-const selectSalesQuery = `SELECT * FROM 
+const selectSalesQuery = `SELECT id, date FROM 
 StoreManager.sales`;
 
+const selectSaleByIdQuery = `SELECT date FROM 
+StoreManager.sales WHERE id = ?`;
+
 const saleModel = {
-  // ::TODO pegar todas as vendas
+  // ::DONE pegar todas as vendas
   getSales: async () => {
     const [result] = await connection.execute(selectSalesQuery);
     return result;
   },
-  // ::TODO pegar veda pelo id
+  // ::DONE pegar veda pelo id
+  getSaleById: async (saleId) => {
+    const [result] = await connection.execute(selectSaleByIdQuery, saleId);
+    return result;
+  },
 };
 
 module.exports = saleModel;
