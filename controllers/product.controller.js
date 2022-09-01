2fc10a19a16afa167f0e8763a8f1454cfea32beb
Jazyel99 <jaziel@outlook.com>
2022-09-01 17:31:14 -0300

    feat: add getProducts in controllers layer

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -3,8 +3,13 @@ const { productService } = require('../services/product.service');
 module.exports.productController = {
   getProductByID: async (req, res) => {
     const { id } = req.params;
-    const response = await productService.getProductByID(id);
-    if (response === null) return res.status(404).json({ message: 'Product not found' });
-    return res.status(200).json(response);
+    const result = await productService.getProductByID(id);
+    if (result === null) return res.status(404).json({ message: 'Product not found' });
+    return res.status(200).json(result);
+  },
+  getProducts: async (req, res) => {
+    const result = await productService.getProducts();
+    if (result === null) return res.status(404).json({ message: 'Product not found' });
+    return res.status(200).json(result);
   },
 };
