ffb91433b59c79f773935d5579a5d7f28430679d
Jazyel99 <jaziel@outlook.com>
2022-09-01 20:30:00 -0300

    test: test getSaleById in models layer

--- tests/unit/models/Sale.test.js
+++ tests/unit/models/Sale.test.js
@@ -20,16 +20,29 @@ const expectedSales = [
   }
 ]
 
+const expectedSaleId1 = [
+  {
+    "date": "2021-09-09T04:54:29.000Z",
+    "productId": 1,
+    "quantity": 2
+  },
+  {
+    "date": "2021-09-09T04:54:54.000Z",
+    "productId": 2,
+    "quantity": 2
+  }
+]
+
+
 // ::TODO teste da camda models
 describe('Testes da camada models [Sales] ', () => {
+  afterEach(() => {
+    Sinon.restore();
+  });
   // ::TODO testes bemsucedidos
   describe('Testes bemsucedidos', () => {
-    afterEach(() => {
-      Sinon.restore();
-    });
     // ::DONE testes da função getSales
     describe('Teste da função getSales', () => {
-
       // ::DONE testar se a função retorna um array não vázio
       it(`Deve retorna uma array não vázio`, async () => {
         Sinon.stub(connection, 'execute').resolves([[expectedSales]]);
@@ -45,6 +58,20 @@ describe('Testes da camada models [Sales] ', () => {
         expect(result).to.equal(expectedSales);
       });
     });
+    describe('Teste da função getSaleById', () => {
+      it('Deve retorna uma array não vázio', async () => {
+        Sinon.stub(connection, 'execute').resolves([[expectedSaleId1]]);
+
+        const [result] = await saleModel.getSaleById(1);
+        expect(result).to.be.an('array').that.not.empty;
+      });
+      it('O objeto deve conter as chaves; data, productId e quantity', async () => {
+        Sinon.stub(connection, 'execute').resolves([[expectedSaleId1]]);
+
+        const [result] = await saleModel.getSaleById(1);
+        expect(result[0]).have.all.keys('date', 'productId', 'quantity');
+      })
+    });
   })
   // ::TODO testes malsucedidos
   describe('Testes malsucedidos', () => {
@@ -58,5 +85,13 @@ describe('Testes da camada models [Sales] ', () => {
         expect(result).to.be.empty;
       });
     });
+    describe('Teste da função getSaleById', () => {
+      it('Deve retornar um array vazio caso o id da venda passado não exista', async () => {
+        Sinon.stub(connection, 'execute').resolves([[]]);
+
+        const result = await saleModel.getSaleById(100);
+        expect(result).to.be.empty;
+      });
+    })
   });
 });
